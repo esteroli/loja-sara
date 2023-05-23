86599aa19a692fef0dbe188c9808886942b14f54
esteroli <ester.oli.souz4@gmail.com>
2023-05-23 17:51:11 -0300

    Acréscimo de ícones
    • Ícone de person e cart
    • Arrumei posições, cor, tamanho
    • Ao abrir a barra de pesquisa, os outros elementos

--- script/script.js
+++ script/script.js
@@ -1,29 +1,32 @@
 //Capturando os elementos do DOM
 var pesquisaIcon = document.querySelector(".pesquisa-icon");
+var loginIcon = document.querySelector(".login-icon")
+var cartIcon = document.querySelector(".cart-icon")
 var pesquisaInput = document.getElementById("pesquisa-input");
-var pesquisaBotao = document.getElementById("pesquisa-botao")
+var pesquisaBotao = document.getElementById("pesquisa-botao");
 
 //Evento de clique no ícone de busca
-pesquisaIcon.addEventListener("click", function() {
+pesquisaIcon.addEventListener("click", function(evento) {
+    //Ocultar outros ícones
+    loginIcon.style.display = "none";
+    cartIcon.style.display = "none";
+
+    //Exibir barra de pesquisa
     pesquisaInput.style.display = "block";
     pesquisaBotao.style.display = "inline-block"
 
-    setTimeout(function() {
-        pesquisaInput.classList.remove("hidden");
-        pesquisaBotao.classList.remove("hidden");
-    }, 10) ; //Aguarda um pequeno intervalo para garantir que a transição seja aplicada corretamente
+    evento.stopPropagation(); //Evita que o clique se propague para o documento
 });
 
 //Evento de clique fora da área de pesquisa para esconder a digitação
-document.addEventListener("click", function(evento) {
-    if(!evento.target.matches(".pesquisa-icon") && !evento.target.matches(".pesquisa-input")) {
-        pesquisaInput.classList.add("hidden");
-        pesquisaBotao.classList.add("hidden");
-        setTimeout(function() {
-            pesquisaInput.style.display = "none";
-            pesquisaBotao.style.display = "none";
-        }, 300); //Aguarda a duração da transição para ocultar os elementos completamente  
-    }
+document.addEventListener("click", function() {
+    //exibir outros ícones
+    loginIcon.style.display = "inline-block";
+    cartIcon.style.display = "inline-block";
+
+    //ocultar barra de pesquisa
+    pesquisaInput.style.display = "none"
+    pesquisaBotao.style.display = "none"
 });
 
 //Evitando que o clique na área de pesquisa feche a aba
